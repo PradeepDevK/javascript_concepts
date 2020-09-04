81880ee8ff1f8368041f9f13264dc449b2ae12dc
pradeeprajkk <pradeepjack555@gmail.com>
2020-09-04 18:26:43 +0530

    ways of combine string

---
+++ 4ways to combine strings/combineStrings.js
@@ -0,0 +1,13 @@
+let icon = 'Jhon';
+
+//Template String
+console.log(`hi ${icon}`);
+
+//join
+console.log(['hi', icon].join(' '));
+
+//concat
+console.log(''.concat('hi ', icon));
+
+//+
+console.log('hi ' + icon);
